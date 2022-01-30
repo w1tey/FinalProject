e4a2ef7edebec843d067e11a6a05b3d94ec0c7e2
w1tey <86645402+w1tey@users.noreply.github.com>
2022-01-30 20:26:05 +0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,90 @@
+﻿using FinalProjectSharp;
+using System;
+using System.IO.Enumeration;
+using System.Threading.Channels;
+
+namespace FinalProject;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, welcome to Translator, a useless app." +
+                          "\nPlease enter 2 languages (from and to) to create a new dictionary");
+        var from = Console.ReadLine();
+        var to = Console.ReadLine();
+        var dictionary = new Dictionary(from, to);
+
+        Console.WriteLine("Now, you will add words to your dictionary.");
+
+        // Add words to dictionary
+        while (true)
+        {
+            Console.WriteLine("1 - Continue" +
+                              "\n2 - Enough words");
+            var choice = Console.ReadLine();
+
+            if (choice == "1")
+            {
+                List<string> translationsForWord = new List<string>();
+                Console.Write("Enter the word you want to add:  ");
+                var word = Console.ReadLine();
+
+                while (true)
+                {
+                    Console.WriteLine("1 - Continue" +
+                                      "\n2 - Enough Translations");
+                    choice = Console.ReadLine();
+
+                    if (choice == "1")
+                    {
+                        Console.WriteLine("Enter word: ");
+                        string? newTranslation = Console.ReadLine();
+                        if (newTranslation != null) translationsForWord.Add(newTranslation);
+                    }
+
+                    else
+                    {
+                        break;
+                    }
+                }
+
+                dictionary.Append(new Translation(word, translationsForWord));
+            }
+
+            else
+            {
+                break;
+            }
+        }
+
+        Console.Clear();
+        Console.WriteLine("Now see all of your translations.");
+        dictionary.Print();
+
+        Dictionary.Serialization(dictionary.Words);
+
+
+
+        while (true)
+        {
+            Console.WriteLine("\nChoose one of these options:" +
+                              "\n\t1 - Add a new translation to dictionary" +
+                              "\n\t2 - Remove a word" +
+                              "\n\t3 - Change a word / its translation" +
+                              "\n\t4 - Print all the words" +
+                              "\n\t5 - Exit");
+
+            var choice = Console.ReadLine();
+
+            switch (choice)
+            {
+                default:
+                    break;
+            }
+
+            break;
+        }
+
+    }
+}
---
+++ dictionary.cs
@@ -0,0 +1,102 @@
+﻿using System.ComponentModel;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace FinalProjectSharp;
+
+public class Dictionary
+{
+    public List<Translation> Words { get; set; }
+    private string? FromLanguage { get; set; }
+    private string? ToLanguage { get; set; }
+
+    public Dictionary(string? fromLanguage, string? toLanguage)
+    {
+        Words = new List<Translation>();
+        FromLanguage = fromLanguage;
+        ToLanguage = toLanguage;
+    }
+
+    public void Print()
+    {
+        Console.WriteLine($"From: {FromLanguage}" +
+                          $"\nTo: {ToLanguage}");
+
+        foreach (var translation in Words)
+        {
+            translation.Print();
+        }
+    }
+
+    public void Append(Translation appendedTranslation)
+    {
+        Words.Add(appendedTranslation);
+    }
+
+    public void Delete(int position)
+    {
+        Words.RemoveAt(position);
+    }
+
+    public static void Serialization(List<Translation> dictionary)
+    {
+        using FileStream fs = new("translation.json", FileMode.OpenOrCreate);
+        JsonSerializer.Serialize(fs, dictionary);
+
+        fs.Close();
+    }
+
+    public static List<Translation> Deserialization()
+    {
+        using FileStream fs = new("translation.json", FileMode.OpenOrCreate);
+        var newDictionary = JsonSerializer.Deserialize<List<Translation>>(fs);
+        fs.Close();
+
+        return newDictionary;
+    }
+}
+
+
+public class Translation
+{
+    private string Word { get; set; }
+    private List<string> Translations;
+
+    [JsonConstructor] public Translation(string word, List<string> translations)
+    {
+        Word = word;
+        Translations = translations;
+    }
+
+    public void Print()
+    {
+        Console.Write($"\nWord - {Word}" +
+                      $"\nTranslations: ");
+
+        foreach (var word in Translations)
+        {
+            Console.Write($"{word} , ");
+        }
+    }
+
+    public void AddTranslation(string transl)
+    {
+        Translations.Add(transl);
+    }
+
+    public void DeleteTranslation(int position)
+    {
+        Translations.RemoveAt(position);
+    }
+
+    public void ChangeTranslation(int position, string newTranslation)
+    {
+        Translations[position] = newTranslation;
+    }
+
+    public void ChangeWord(int position, string newWord)
+    {
+        Word = newWord;
+    }
+
+}
